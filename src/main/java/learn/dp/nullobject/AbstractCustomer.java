b49f240a712702694ee962813b7e0261bf2fb039
septanto <siprianus.septanto@doku.com>
2018-07-11 09:56:16 +0700

    learned null object and strategy. Migrate to maven

---
+++ src/main/java/learn/dp/nullobject/AbstractCustomer.java
@@ -0,0 +1,9 @@
+package learn.dp.nullobject;
+
+public abstract class AbstractCustomer {
+
+    protected String name;
+    public abstract boolean isNil();
+    public abstract String getName();
+
+}
